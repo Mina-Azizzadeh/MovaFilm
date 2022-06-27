483419bfb552345481af995344c6cbfd3cfd3f19
minae <mina.azizzade78@gmail.comm>
2022-06-27 16:36:09 +0430

    first page just responsive phone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,8 @@
 import { Component } from '@angular/core';
-import {
-  trigger,
-  state,
-  style,
-  transition,
-  animate,
-} from '@angular/animations';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  animations: [
-    trigger('list2', [
-      transition('void => *', [
-        style({
-          opacity: 0,
-          transform: 'scale(0.5)',
-        }),
-        animate(500),
-      ]),
-    ]),
-  ],
 })
 export class AppComponent {
   title = 'Mova';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
